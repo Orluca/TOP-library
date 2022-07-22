22b435a0dca8ddfa298fad916a53640c50bf1e73
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-22 17:47:48 +0200

    First basic version of displayBooks function

--- script.js
+++ script.js
@@ -13,7 +13,37 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function displayBooks() {}
-
 addBookToLibrary(new Book("Lord of the Rings - The Fellowship of the Ring", "JRR Tolkien", 1954, true));
-addBookToLibrary(new Book("Harry Potter 1", "JK Rowling", 1997, true));
+addBookToLibrary(new Book("Harry Potter 1", "JK Rowling", 1997, false));
+
+function displayBooks(libraryArray) {
+  libraryArray.forEach((book) => {
+    const bookContainer = document.querySelector(".book-list-container");
+
+    bookContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="book-item-container">
+        <div class="book-property-container">
+          <div class="label">Title:</div>
+          <div class="title-value">${book.title}</div>
+        </div>
+        <div class="book-property-container">
+          <div class="label">Author:</div>
+          <div class="author-value">${book.author}</div>
+        </div>
+        <div class="book-property-container">
+          <div class="label">Year:</div>
+          <div class="year-value">${book.year}</div>
+        </div>
+        <div class="book-property-container">
+          <div class="label">Read:</div>
+          <input type="checkbox" name="" id="" ${book.isRead ? "checked" : ""}/>
+        </div>
+      </div>
+    `
+    );
+  });
+}
+
+displayBooks(myLibrary);
