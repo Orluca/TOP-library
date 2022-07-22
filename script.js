94174bf81d2eecd10c9dbec174931f83a02377d3
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-22 19:03:30 +0200

    Added localStorage

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+"use strict";
+
+// ###############################################################
+// ####################### GLOBAL VARIABLES ######################
+// ###############################################################
 let myLibrary = [];
 
+// ###############################################################
+// #################### HTML ELEMENT SELECTORS ###################
+// ###############################################################
+const $btnAddBook = document.querySelector(".add-book-button");
+const $containerAddBook = document.querySelector(".add-book-container");
+
+// ###############################################################
+// ########################### CLASSES ###########################
+// ###############################################################
 class Book {
   constructor(title, author, year, isRead) {
     this.title = title;
@@ -9,17 +23,44 @@ class Book {
   }
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+// ###############################################################
+// ########################## FUNCTIONS ##########################
+// ###############################################################
+function saveToLocalStorage() {
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
 }
 
-addBookToLibrary(new Book("Lord of the Rings - The Fellowship of the Ring", "JRR Tolkien", 1954, true));
-addBookToLibrary(new Book("Harry Potter 1", "JK Rowling", 1997, false));
+function addBookToLibrary() {
+  const title = document.querySelector("#title-input");
+  const author = document.querySelector("#author-input");
+  const year = document.querySelector("#year-input");
+  const isRead = document.querySelector("#read-status-input");
+
+  // Check if all inputs are filled out
+  if (!title.value || !author.value || !year.value) {
+    console.log("Please fill out everything");
+    return;
+  }
+
+  // Add the book to the myLibrary array
+  myLibrary.push(new Book(title.value, author.value, year.value, isRead.checked));
+
+  // Reset inputs
+  title.value = author.value = year.value = "";
+  isRead.checked = false;
+
+  // Update display to show the newly added book on the GUI
+  displayBooks(myLibrary);
+
+  // Save the current myLibrary array in localStorage
+  saveToLocalStorage();
+}
 
 function displayBooks(libraryArray) {
-  libraryArray.forEach((book) => {
-    const bookContainer = document.querySelector(".book-list-container");
+  const bookContainer = document.querySelector(".book-items-container");
+  bookContainer.innerHTML = "";
 
+  libraryArray.forEach((book) => {
     bookContainer.insertAdjacentHTML(
       "beforeend",
       `
@@ -46,4 +87,21 @@ function displayBooks(libraryArray) {
   });
 }
 
-displayBooks(myLibrary);
+function init() {
+  const savedLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+  if (savedLibrary) myLibrary = savedLibrary;
+
+  displayBooks(myLibrary);
+}
+
+// ###############################################################
+// ####################### EVENT LISTENERS #######################
+// ###############################################################
+
+$btnAddBook.addEventListener("click", addBookToLibrary);
+$containerAddBook.addEventListener("keypress", function (e) {
+  if (e.key !== "Enter") return;
+  addBookToLibrary();
+});
+
+init();
