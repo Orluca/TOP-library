7f12a959a99ea25ee0069e62af7771e0b951f73b
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-24 19:30:03 +0200

    Tweaks to the Edit window

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 "use strict";
 
+// import { defaultBooks } from "./default_books.js";
+
+// A list of default books, to gain a better understanding of how the app works, without having to add a bunch of books yourself
+// const myLibrary = [
+//   { title: "A Clockwork Orange", author: "Burgess, Anthony", year: 1962, isRead: false },
+//   { title: "The Fellowship of the Ring", author: "Tolkien, J.R.R.", year: 1954, isRead: true },
+//   { title: "The Two Towers", author: "Tolkien, J.R.R.", year: 1954, isRead: true },
+//   { title: "The Return of the King", author: "Tolkien, J.R.R.", year: 1955, isRead: true },
+//   { title: "The Great Gatsby", author: "Fitzgerald, F. Scott", year: 1925, isRead: false },
+//   { title: "Moby Dick", author: "Melville, Herman", year: 1851, isRead: false },
+//   { title: "Typee", author: "Melville, Herman", year: 1846, isRead: false },
+//   { title: "Billy Budd", author: "Melville, Herman", year: 1924, isRead: false },
+//   { title: "Typee", author: "Melville, Herman", year: 1846, isRead: false },
+//   { title: "The Stand", author: "King, Stephen", year: 1978, isRead: true },
+//   { title: "Firestarter", author: "King, Stephen", year: 1980, isRead: false },
+//   { title: "Pet Sematary", author: "King, Stephen", year: 1983, isRead: true },
+//   { title: "Doctor Sleep", author: "King, Stephen", year: 2013, isRead: true },
+//   { title: "Steelheart", author: "Sanderson, Brandon", year: 2013, isRead: true },
+//   { title: "The Way of Kings", author: "Sanderson, Brandon", year: 2010, isRead: true },
+// ];
+
 // ###############################################################
 // ####################### GLOBAL VARIABLES ######################
 // ###############################################################
@@ -15,10 +36,10 @@ const $containerAddBook = document.querySelector(".add-book-container");
 const $containerBookList = document.querySelector(".book-items-container");
 
 const $modalWindow = document.querySelector(".edit-modal-window");
-const $modalTitle = document.querySelector(".modal-input-title");
-const $modalAuthor = document.querySelector(".modal-input-author");
-const $modalYear = document.querySelector(".modal-input-year");
-const $modalReadStatus = document.querySelector(".modal-input-read-status");
+const $modalTitle = document.querySelector("#modal-input-title");
+const $modalAuthor = document.querySelector("#modal-input-author");
+const $modalYear = document.querySelector("#modal-input-year");
+const $modalReadStatus = document.querySelector("#modal-input-read-status");
 
 // ###############################################################
 // ########################### CLASSES ###########################
@@ -312,8 +333,14 @@ window.addEventListener("click", function (e) {
 });
 
 const $btnDarkMode = document.querySelector(".dark-mode-btn");
+const $btnDeleteAll = document.querySelector(".delete-all-btn");
 
 $btnDarkMode.addEventListener("click", function () {
   console.log("Lkjsdf");
   document.body.classList.toggle("dark-mode");
+});
+
+$btnDeleteAll.addEventListener("click", function () {
+  deleteAllBooks();
+  displayBooks();
 });
