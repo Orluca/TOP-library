de9173ebfc5889601f80a2356bfa1242505ed320
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-24 20:47:50 +0200

    sticky titlebar for book list

--- script.js
+++ script.js
@@ -16,6 +16,20 @@ const defaultBooks = [
   { title: "Doctor Sleep", author: "King, Stephen", year: 2013, isRead: true },
   { title: "Steelheart", author: "Sanderson, Brandon", year: 2013, isRead: true },
   { title: "The Way of Kings", author: "Sanderson, Brandon", year: 2010, isRead: true },
+  { title: "A Clockwork Orange", author: "Burgess, Anthony", year: 1962, isRead: false },
+  { title: "The Fellowship of the Ring", author: "Tolkien, J.R.R.", year: 1954, isRead: true },
+  { title: "The Two Towers", author: "Tolkien, J.R.R.", year: 1954, isRead: true },
+  { title: "The Return of the King", author: "Tolkien, J.R.R.", year: 1955, isRead: true },
+  { title: "The Great Gatsby", author: "Fitzgerald, F. Scott", year: 1925, isRead: false },
+  { title: "Moby Dick", author: "Melville, Herman", year: 1851, isRead: false },
+  { title: "Typee", author: "Melville, Herman", year: 1846, isRead: false },
+  { title: "Billy Budd", author: "Melville, Herman", year: 1924, isRead: false },
+  { title: "The Stand", author: "King, Stephen", year: 1978, isRead: true },
+  { title: "Firestarter", author: "King, Stephen", year: 1980, isRead: false },
+  { title: "Pet Sematary", author: "King, Stephen", year: 1983, isRead: true },
+  { title: "Doctor Sleep", author: "King, Stephen", year: 2013, isRead: true },
+  { title: "Steelheart", author: "Sanderson, Brandon", year: 2013, isRead: true },
+  { title: "The Way of Kings", author: "Sanderson, Brandon", year: 2010, isRead: true },
 ];
 
 // ###############################################################
