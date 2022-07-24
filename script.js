057aa50322399f2145f75a10be745bade85d5769
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-07-24 19:46:10 +0200

    Added default list of books

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 "use strict";
 
-// import { defaultBooks } from "./default_books.js";
-
 // A list of default books, to gain a better understanding of how the app works, without having to add a bunch of books yourself
-// const myLibrary = [
-//   { title: "A Clockwork Orange", author: "Burgess, Anthony", year: 1962, isRead: false },
-//   { title: "The Fellowship of the Ring", author: "Tolkien, J.R.R.", year: 1954, isRead: true },
-//   { title: "The Two Towers", author: "Tolkien, J.R.R.", year: 1954, isRead: true },
-//   { title: "The Return of the King", author: "Tolkien, J.R.R.", year: 1955, isRead: true },
-//   { title: "The Great Gatsby", author: "Fitzgerald, F. Scott", year: 1925, isRead: false },
-//   { title: "Moby Dick", author: "Melville, Herman", year: 1851, isRead: false },
-//   { title: "Typee", author: "Melville, Herman", year: 1846, isRead: false },
-//   { title: "Billy Budd", author: "Melville, Herman", year: 1924, isRead: false },
-//   { title: "Typee", author: "Melville, Herman", year: 1846, isRead: false },
-//   { title: "The Stand", author: "King, Stephen", year: 1978, isRead: true },
-//   { title: "Firestarter", author: "King, Stephen", year: 1980, isRead: false },
-//   { title: "Pet Sematary", author: "King, Stephen", year: 1983, isRead: true },
-//   { title: "Doctor Sleep", author: "King, Stephen", year: 2013, isRead: true },
-//   { title: "Steelheart", author: "Sanderson, Brandon", year: 2013, isRead: true },
-//   { title: "The Way of Kings", author: "Sanderson, Brandon", year: 2010, isRead: true },
-// ];
+const defaultBooks = [
+  { title: "A Clockwork Orange", author: "Burgess, Anthony", year: 1962, isRead: false },
+  { title: "The Fellowship of the Ring", author: "Tolkien, J.R.R.", year: 1954, isRead: true },
+  { title: "The Two Towers", author: "Tolkien, J.R.R.", year: 1954, isRead: true },
+  { title: "The Return of the King", author: "Tolkien, J.R.R.", year: 1955, isRead: true },
+  { title: "The Great Gatsby", author: "Fitzgerald, F. Scott", year: 1925, isRead: false },
+  { title: "Moby Dick", author: "Melville, Herman", year: 1851, isRead: false },
+  { title: "Typee", author: "Melville, Herman", year: 1846, isRead: false },
+  { title: "Billy Budd", author: "Melville, Herman", year: 1924, isRead: false },
+  { title: "Typee", author: "Melville, Herman", year: 1846, isRead: false },
+  { title: "The Stand", author: "King, Stephen", year: 1978, isRead: true },
+  { title: "Firestarter", author: "King, Stephen", year: 1980, isRead: false },
+  { title: "Pet Sematary", author: "King, Stephen", year: 1983, isRead: true },
+  { title: "Doctor Sleep", author: "King, Stephen", year: 2013, isRead: true },
+  { title: "Steelheart", author: "Sanderson, Brandon", year: 2013, isRead: true },
+  { title: "The Way of Kings", author: "Sanderson, Brandon", year: 2010, isRead: true },
+];
 
 // ###############################################################
 // ####################### GLOBAL VARIABLES ######################
@@ -148,7 +146,12 @@ function editEntry(id, element) {
 
 function init() {
   const savedLibrary = JSON.parse(localStorage.getItem("myLibrary"));
-  if (savedLibrary) myLibrary = savedLibrary;
+
+  if (savedLibrary.length === 0) {
+    myLibrary = defaultBooks;
+  } else {
+    myLibrary = savedLibrary;
+  }
 
   displayBooks(myLibrary);
 }
@@ -308,9 +311,15 @@ function getNumberOfReadBooks() {
   return myLibrary.reduce((acc, book) => (acc += book.isRead ? 1 : 0), 0);
 }
 
+function closeSettingsModal() {
+  $settingsModal.classList.add("hidden");
+}
+
 function deleteAllBooks() {
   if (!confirm("This will delete ALL books from your library. Are you sure you want to continue?")) return;
-  localStorage.clear();
+  myLibrary = [];
+  saveToLocalStorage();
+  closeSettingsModal();
 }
 
 const $settingsModal = document.querySelector(".settings-modal");
